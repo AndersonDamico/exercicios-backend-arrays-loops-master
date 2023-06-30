3a600d3880ec71f4913c1af4cc7f11a05b817bdb
AndersonDamico <andersondamico2@gmail.com>
2023-06-30 10:35:37 -0300

    exercicios de 1 á 4

--- 02/index.js
+++ 02/index.js
@@ -1,1 +1,25 @@
-const letras = ["A", "a", "B", "C", "E", "e"];
+const letras = ["A", "a", "B", "C", "L", "z"];
+
+let encontrado = false;
+
+for (let letra of letras){
+    if(letra === 'e'){
+        console.log('tem a letra "e"');
+        encontrado = true;
+        break;
+    }
+}if (!encontrado) {
+    console.log('Nenhuma letra "e" foi encontrada.')
+}
+
+
+
+const letras1 = ["A", "a", "B", "C", "E", "e"];
+
+let encontradas = 0;
+
+for (let vogais of letras1){
+    if(vogais ==='E'|| vogais ==='e'){
+        encontradas++;  
+    }
+}console.log(`Foram encontradas ${encontradas} letras "E" ou "e"`)
--- 03/index.js
+++ 03/index.js
@@ -1,2 +1,12 @@
-const numeros = [54, 22, 14, 87, 10, 284];
+const numeros = [54, 22, 14, 87, 284];
+
+console.log (numeros.indexOf(10));
+
+
+        
+const numeros1 = [54, 22, 14, 10, 284];
+console.log (numeros1.indexOf(10));   
+       
+
+
 
